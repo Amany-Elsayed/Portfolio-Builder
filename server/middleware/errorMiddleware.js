79121b836b258bb67938c78bf692ca91b44e84bc
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-06-17 17:25:06 +0300

    improved error handler

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -4,11 +4,15 @@ const notFound = (req, res, next) => {
 }
 
 const errorHandler = (err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err)
+    }
+
     const statusCode = err.statusCode || res.statusCode || 500
 
     res.status(statusCode).json({
         message: err.message,
-        stack: process.env.NODE_ENV === "production" ? null : err.stack
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })
 }
 
