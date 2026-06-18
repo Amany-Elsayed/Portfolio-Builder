6f351b89fe4f1e70589914893275accf80e84093
Amany-Elsayed <amanyelsayed0@outlook.com>
2026-06-18 11:39:22 +0300

    prettier

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -1,16 +1,33 @@
-const express = require('express')
-const { createOrUpdateProtfolio, getMyPortfolio, getPublicPortfolio, uploadProfileImage, uploadProjectImages } = require('../controllers/portfolioController')
-const { protect } = require('../middleware/authMiddleware')
-const upload = require("../middleware/uploadMiddleware")
+const express = require("express");
+const {
+  createOrUpdateProtfolio,
+  getMyPortfolio,
+  getPublicPortfolio,
+  uploadProfileImage,
+  uploadProjectImages,
+} = require("../controllers/portfolioController");
+const { protect } = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/')
-    .post(protect, createOrUpdateProtfolio)
-    .put(protect, createOrUpdateProtfolio)
-router.post('/upload-profile-image', protect, upload.single('image'), uploadProfileImage)
-router.post('/projects/:projectId/images', protect, upload.array('images', 5), uploadProjectImages)
-router.get('/me', protect, getMyPortfolio)
-router.get('/:username', getPublicPortfolio)
+router
+  .route("/")
+  .post(protect, createOrUpdateProtfolio)
+  .put(protect, createOrUpdateProtfolio);
+router.post(
+  "/upload-profile-image",
+  protect,
+  upload.single("image"),
+  uploadProfileImage,
+);
+router.post(
+  "/projects/:projectId/images",
+  protect,
+  upload.array("images", 5),
+  uploadProjectImages,
+);
+router.get("/me", protect, getMyPortfolio);
+router.get("/:username", getPublicPortfolio);
 
-module.exports = router
+module.exports = router;
